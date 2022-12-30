7e3e6573b666582fa9de30fca47ee039b423e8e9
Soubhik Kumar Gon <119437069+mogulcoder26@users.noreply.github.com>
2022-12-30 14:49:51 +0530

    Create swg_in.c

---
+++ swg_in.c
@@ -0,0 +1,68 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+int main()
+{
+    int compturn, a;
+    char userturn;
+    while (2 > 1)
+    {
+        srand(time(0));
+        compturn = rand() % 3 + 1;
+        
+        printf("Snake(s)/water(w)/gun(g)?:\n");
+        scanf("%c", &userturn);
+
+        if (userturn == 's')
+        {
+            a = 1;
+        }
+        else if (userturn == 'w')
+        {
+            a = 2;
+        }
+        else if (userturn == 'g')
+        {
+            a = 3;
+        }
+
+        else if (compturn == 1 && a == 1)
+        {
+            printf("tie!computer choosed %c\n", 's');
+        }
+        else if (compturn == 2 && a == 2)
+        {
+            printf("tie!computer choosed %c\n", 'w');
+        }
+        else if (compturn == 3 && a == 3)
+        {
+            printf("tie!computer choosed %c\n", 'g');
+        }
+
+        else if (compturn == 1 && a == 2)
+        {
+            printf("you lose computer choosed %c\n", 's');
+        }
+        else if (compturn == 1 && a == 3)
+        {
+            printf("You win computer choosed %c\n", 's');
+        }
+        else if (compturn == 2 && a == 1)
+        {
+            printf("you win computer choosed %c\n", 'w');
+        }
+        else if (compturn == 2 && a == 3)
+        {
+            printf("you lose computer choosed %c\n", 'w');
+        }
+        else if (compturn == 3 && a == 1)
+        {
+            printf("you lose computer choosed %c\n", 'g');
+        }
+        else if (compturn == 3 && a == 2)
+        {
+            printf("you win computer choosed %c\n", 'g');
+        }
+    }
+    return 0;
+}
